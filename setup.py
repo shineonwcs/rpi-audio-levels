84c522a43e6acb5347524c1d9c7b41ee208883df
shineonwcs <justin@shineon.biz>
2024-09-28 19:37:21 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,35 +4,36 @@ from Cython.Distutils import build_ext
 from distutils.core import setup
 from distutils.extension import Extension
 
-gpu_fft_src_path = '/opt/vc/src/hello_pi/hello_fft/'  # gpu fft is directly included in Raspbian
-gpu_fft_source_files = ('gpu_fft_base.c', 'gpu_fft.c', 'gpu_fft_shaders.c',
-                        'gpu_fft_twiddles.c', 'gpu_fft_trans.c', 'mailbox.c')
+# Remove the GPU FFT source files (gpu_fft_source_files)
+# and path (gpu_fft_src_path)
 
-sources = ['src/rpi_audio_levels.pyx',
-           'src/_rpi_audio_levels.c']
+# Keep your Python and C source files
+sources = ['src/rpi_audio_levels.pyx', 'src/_rpi_audio_levels.c']
 
-sources.extend([gpu_fft_src_path + p for p in gpu_fft_source_files])
-
-README = """Python binding allowing to retrieve audio levels by frequency bands given 
-audio samples (power spectrum in fact), on a Raspberry Pi, using GPU FFT"""
+# Update the README text
+README = """Python binding allowing to retrieve audio levels by frequency bands given
+audio samples (power spectrum in fact), on a Raspberry Pi, using FFTW"""
 
 setup(
-    name = "rpi_audio_levels",
-    version = '0.1.1',
-    url = 'https://github.com/colin-guyon/rpi-audio-levels',
-    author = 'Colin Guyon',
-    description = 'Python binding for Raspberry Pi GPU FFTerface',
-    long_description = README,
+    name="rpi_audio_levels",
+    version='0.1.1',
+    url='https://github.com/colin-guyon/rpi-audio-levels',
+    author='Colin Guyon',
+    description='Python binding for Raspberry Pi FFT using FFTW',
+    long_description=README,
     install_requires=[
-          'cython>=0.19.1',
-          'numpy',
-      ],
+        'cython>=0.19.1',
+        'numpy',
+    ],
     cmdclass={'build_ext': build_ext},
-    include_dirs=[numpy.get_include()],
+    include_dirs=[numpy.get_include(), '/usr/include'],
     ext_modules=[
-        Extension('rpi_audio_levels',
-                  sources=sources,
-                  extra_compile_args=['-I' + gpu_fft_src_path],
-                  extra_link_args=['-lrt', '-lm'])]
+        Extension(
+            'rpi_audio_levels',
+            sources=sources,
+            libraries=['fftw3'],  # Link to FFTW library
+            extra_compile_args=['-O3'],  # Optimization for FFTW
+            extra_link_args=['-lfftw3', '-lm'],  # Link FFTW and math library
+        )
+    ]
 )
-
